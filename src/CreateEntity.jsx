6b0fa50dfc9b03be56bf98440572d4c4a9b49480
tarakona <michaelrosen44@gmail.com>
2024-06-19 23:14:46 +1200

    Set up skeleton of create entity component

---
+++ src/CreateEntity.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import {db} from "./firebase"
+
+const CreateEntity = () => {
+
+}
+
+export default CreateEntity
