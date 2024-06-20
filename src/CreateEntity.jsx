635164113278058eeddf66dc7d9841548b6a214d
tarakona <michaelrosen44@gmail.com>
2024-06-20 22:34:35 +1200

    Reset state variables as the submit handles and fixed a couple of typos

--- src/CreateEntity.jsx
+++ src/CreateEntity.jsx
@@ -5,7 +5,7 @@ import {db} from "./firebase"
 const CreateEntity = () => {
 const [id, setId] = useState ("");
 const [entityClass, setEntityClass] = useState (""); //Class is a keyword, so this field has been renamed to Entity Class but will be referring to the class field in the firebase database
-const [containment, seContainment] = useState ("");
+const [containment, setContainment] = useState ("");
 const [description, setdescription] = useState ("");
 
 const ourCollection = collection(db, "entities");
@@ -14,7 +14,11 @@ const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-        await addDoc(outCollection, {id: id, class: entityClass, containment: containment, description: description})
+        await addDoc(ourCollection, {id: id, class: entityClass, containment: containment, description: description})
+        setId("");
+        setEntityClass("");
+        setContainment("");
+        setdescription("");
     } catch(error) {
         console.error("Error adding entity: ", error)
     }
