7dbfdee42e18c7312f576b3b2175482550e9221d
tarakona <michaelrosen44@gmail.com>
2024-06-20 23:12:50 +1200

    Update component created. Skeleton produced

---
+++ src/UpdateEntity.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
+import { db } from "./firebase.js";
+
+const UpdateEntity = () => {
+return (
+    <>
+    </>
+)
+}
+
+export default UpdateEntity;
