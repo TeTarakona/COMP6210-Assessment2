1eeb94e66153347502839d003c12e248b978b804
tarakona <michaelrosen44@gmail.com>
2024-06-20 22:32:16 +1200

    Wrote most of the function of the create entity component

--- src/CreateEntity.jsx
+++ src/CreateEntity.jsx
@@ -3,7 +3,22 @@ import { collection, addDoc } from "firebase/firestore";
 import {db} from "./firebase"
 
 const CreateEntity = () => {
+const [id, setId] = useState ("");
+const [entityClass, setEntityClass] = useState (""); //Class is a keyword, so this field has been renamed to Entity Class but will be referring to the class field in the firebase database
+const [containment, seContainment] = useState ("");
+const [description, setdescription] = useState ("");
 
+const ourCollection = collection(db, "entities");
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        await addDoc(outCollection, {id: id, class: entityClass, containment: containment, description: description})
+    } catch(error) {
+        console.error("Error adding entity: ", error)
+    }
+}
 }
 
 export default CreateEntity
